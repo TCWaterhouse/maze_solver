f0f6c6872254cfb1037581a759cf00ba6a37937f
TCWaterhouse <thomas_waterhouse@hotmail.co.uk>
2024-08-26 13:34:06 +0100

    Added func to maze class to reset visited data member

--- tests.py
+++ tests.py
@@ -16,32 +16,29 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
-    def test_maze_create_cells_large(self):
-        num_cols = 50
-        num_rows = 60
-        m2 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        self.assertEqual(
-            len(m2._cells),
-            num_cols,
-        )
-        self.assertEqual(
-            len(m2._cells[0]),
-            num_rows,
-        )
-
     def test_maze_break_entrance_and_exit(self):
         num_cols = 10
         num_rows = 10
-        m4 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        m4._break_entrance_and_exit()
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
-            m4._cells[0][0].has_top_wall,
+            m1._cells[0][0].has_top_wall,
             False
         )
         self.assertEqual(
-            m4._cells[m4._num_cols - 1][m4._num_rows - 1].has_bottom_wall,
+            m1._cells[m1._num_cols - 1][m1._num_rows - 1].has_bottom_wall,
             False
         )
+
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                )
 
 if __name__ == "__main__":
     unittest.main()
