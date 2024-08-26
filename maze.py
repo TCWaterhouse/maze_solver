16542b6d45a29a3c3cb005156db428777cf2df57
TCWaterhouse <thomas_waterhouse@hotmail.co.uk>
2024-08-26 12:08:24 +0100

    Added visited data member to cell class

--- maze.py
+++ maze.py
@@ -1,5 +1,5 @@
 from cell import Cell
-import time
+import time, random
 
 
 class Maze:
@@ -12,6 +12,7 @@ class Maze:
             cell_size_x,
             cell_size_y,
             win = None,
+            seed = None
         ):
         self._x1 = x1
         self._y1 = y1
@@ -20,6 +21,9 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        if seed:
+            random.seed(seed)
+
 
         self._create_cells()
         self._break_entrance_and_exit()
